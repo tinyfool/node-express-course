2c5f47bd96ae7a8ebb4027aef4b62b555326003f
HaoPeiqiang <Tinyfool@gmail.com>
2020-02-13 23:03:59 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 const mockUserData = [
 	{name:'Mark'},
 	{name:'Jill'}
@@ -13,6 +17,28 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		users: req.params.id
 	})
+})
+
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
 })
 
 app.listen(8000,function(){
